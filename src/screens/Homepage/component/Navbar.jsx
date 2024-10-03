3b1fa183b4b484d0b08b71e87905d39841b92635
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-10-03 09:55:37 +0530

    update navbar

--- src/screens/Homepage/component/Navbar.jsx
+++ src/screens/Homepage/component/Navbar.jsx
@@ -5,11 +5,11 @@ import { FaUserCircle } from "react-icons/fa"; // Using react-icons for icons
 
 const Navbar = () => {
   return (
-    <nav className="bg-transparent flex items-center border-b-2 border-[#031c3f] fixed top-4 w-full z-10">
-      <div className="container  mx-auto flex justify-between items-center  px-4">
-        <div className="w-[80%] mx-auto flex justify-between items-center ">
+    <nav className="bg-transparent flex items-center border-b-2 border-[#031c3f] fixed top-6 w-full z-10">
+      <div className="container  mx-auto flex justify-between items-center">
+        <div className="w-[90%]  flex justify-between items-center py-2 px-6 border-r-2 border-[#031c3f]">
           {/* Logo */}
-          <div className="items-center">
+          <div className="items-center ml-4">
             <Logo />
           </div>
 
@@ -33,15 +33,7 @@ const Navbar = () => {
           </div>
         </div>
         {/* Track Your Order Button */}
-        <div className="flex items-center w-[20%] justify-end">
-          <a
-            href="#"
-            className="bg-yellow-500 text-white px-4 py-2 rounded-md hover:bg-yellow-600 transition"
-          >
-            Track Your Order
-          </a>
-          <FaUserCircle className="text-white text-2xl ml-4" />
-        </div>
+        <div className="flex items-center w-[20%] justify-end"></div>
       </div>
     </nav>
   );
