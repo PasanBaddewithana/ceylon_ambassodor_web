21d4cefd8ba885c417d09310b6628a276fdbf019
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-10-03 11:43:37 +0530

    update bottom nav section borders

--- src/screens/Homepage/component/NavigationSection.jsx
+++ src/screens/Homepage/component/NavigationSection.jsx
@@ -2,30 +2,30 @@ import React from "react";
 
 const NavSection = () => {
   return (
-    <section className="w-[85%] mx-auto mt-10 grid grid-cols-4 text-center py-4 bg-white ">
-      <div className="relative group cursor-pointer">
+    <section className="w-[85%] mx-auto mt-10 grid grid-cols-4 text-center py-4 bg-white">
+      <div className="relative group cursor-pointer border-b-2 border-[#031c3f] pb-[10px]">
         <span className="text-[#031c3f] font-bold group-hover:text-yellow-500">
           UNITED FREIGHT SOLUTIONS
         </span>
-        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 w-0 h-[2px] bg-[#031c3f] group-hover:w-[80%] group-hover:bg-[#c3a05f] transition-all duration-300"></div>
+        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-[-2px] w-0 h-[3px] bg-[#031c3f] group-hover:w-[80%] group-hover:bg-[#c3a05f] transition-all duration-300"></div>
       </div>
-      <div className="relative group cursor-pointer">
+      <div className="relative group cursor-pointer border-b-2 border-[#031c3f] pb-[10px]">
         <span className="text-[#031c3f] font-bold group-hover:text-yellow-500">
           KHMERGATE
         </span>
-        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 w-0 h-[2px] bg-[#031c3f] group-hover:w-[80%] group-hover:bg-[#c3a05f] transition-all duration-300"></div>
+        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-[-2px] w-0 h-[3px] bg-[#031c3f] group-hover:w-[80%] group-hover:bg-[#c3a05f] transition-all duration-300"></div>
       </div>
-      <div className="relative group cursor-pointer">
+      <div className="relative group cursor-pointer border-b-2 border-[#031c3f] pb-[10px]">
         <span className="text-[#031c3f] font-bold group-hover:text-yellow-500">
           AEROFLY AVIATION
         </span>
-        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 w-0 h-[2px] bg-[#031c3f] group-hover:w-[80%] group-hover:bg-[#c3a05f] transition-all duration-300"></div>
+        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-[-2px] w-0 h-[3px] bg-[#031c3f] group-hover:w-[80%] group-hover:bg-[#c3a05f] transition-all duration-300"></div>
       </div>
-      <div className="relative group cursor-pointer">
+      <div className="relative group cursor-pointer border-b-2 border-[#031c3f] pb-[10px]">
         <span className="text-[#031c3f] font-bold group-hover:text-yellow-500">
           BRANDPULSE
         </span>
-        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 w-0 h-[2px] bg-[#031c3f] group-hover:w-[80%] group-hover:bg-[#c3a05f] transition-all duration-300"></div>
+        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-[-2px] w-0 h-[3px] bg-[#031c3f] group-hover:w-[80%] group-hover:bg-[#c3a05f] transition-all duration-300"></div>
       </div>
     </section>
   );
