559c466fbb3ce6570f60767aaaa6717ad8eb3253
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-10-03 13:05:07 +0530

    add image courasal

--- src/screens/Homepage/component/HeroSection.jsx
+++ src/screens/Homepage/component/HeroSection.jsx
@@ -1,17 +1,35 @@
 import React from "react";
-import planeImage from "../../../assets/editPlaneImage.png";
+import Slider from "react-slick"; // Import Slider component from react-slick
+import "slick-carousel/slick/slick.css"; // Import slick-carousel styles
+import "slick-carousel/slick/slick-theme.css"; // Import slick-theme styles
 
-const Hero = () => {
+const Hero = ({ images }) => {
+  // Slider settings
+  const settings = {
+    dots: true, // Show dots for navigation
+    infinite: true, // Loop the slides
+    speed: 500, // Transition speed
+    slidesToShow: 1, // Number of slides to show at once
+    slidesToScroll: 1, // Number of slides to scroll at once
+    autoplay: true, // Enable autoplay
+    autoplaySpeed: 3000, // Autoplay speed in milliseconds
+  };
+
   return (
     <section className="relative">
       <div className="w-full">
-        <img
-          src={planeImage}
-          alt="Airplane"
-          className="w-full h-[570px] object-cover" // Keep object-cover to fill the space
-        />
-        {/* White overlay with opacity */}
-        <div className="absolute inset-0 bg-white opacity-10"></div>
+        <Slider {...settings}>
+          {images.map((image, index) => (
+            <div key={index} className="relative">
+              <img
+                src={image}
+                alt={`Airplane ${index + 1}`}
+                className="w-full h-[570px] object-cover" // Keep object-cover to fill the space
+              />
+              {/* White overlay with opacity */}
+            </div>
+          ))}
+        </Slider>
       </div>
     </section>
   );
--- src/screens/Homepage/screen/HomePage.jsx
+++ src/screens/Homepage/screen/HomePage.jsx
@@ -4,13 +4,16 @@ import Hero from "../component/HeroSection";
 import NavSection from "../component/NavigationSection";
 import TopHeader from "../component/TopHeader";
 import Navbar from "../component/Navbar";
+import PlanImage from "../../../assets/editPlaneImage.png";
+import CargoService from "../../../assets/cargo-service-image.svg";
 
 function HomePage() {
+  const images = [PlanImage, CargoService];
   return (
     <div>
       <TopHeader />
       <Navbar />
-      <Hero />
+      <Hero images={images} />
       <NavSection />
     </div>
   );
