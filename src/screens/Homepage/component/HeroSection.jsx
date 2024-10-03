751149bdc5afad6d55f96015b0d246db5dc1b4f6
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-10-03 13:38:51 +0530

    add cutom next arrow and prev arrow for image courasal

--- src/screens/Homepage/component/HeroSection.jsx
+++ src/screens/Homepage/component/HeroSection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick"; // Import Slider component from react-slick
 import "slick-carousel/slick/slick.css"; // Import slick-carousel styles
 import "slick-carousel/slick/slick-theme.css"; // Import slick-theme styles
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa"; // Import icons for arrows
 
 const Hero = ({ images }) => {
   // Slider settings
@@ -13,15 +14,13 @@ const Hero = ({ images }) => {
     slidesToScroll: 1, // Number of slides to scroll at once
     autoplay: true, // Enable autoplay
     autoplaySpeed: 3000, // Autoplay speed in milliseconds
+    nextArrow: <CustomNextArrow />, // Custom next arrow
+    prevArrow: <CustomPrevArrow />, // Custom previous arrow
   };
 
   return (
     <section className="relative overflow-hidden">
-      {" "}
-      {/* Added overflow-hidden */}
       <div className="w-full">
-        {" "}
-        {/* Ensure full width */}
         <Slider {...settings}>
           {images.map((image, index) => (
             <div key={index} className="relative">
@@ -31,11 +30,38 @@ const Hero = ({ images }) => {
                 className="w-full h-[570px] object-cover" // Keep object-cover to fill the space
               />
               {/* White overlay with opacity */}
+              <div className="absolute inset-0 bg-white opacity-10"></div>
             </div>
           ))}
         </Slider>
       </div>
     </section>
+  );
+};
+
+// Custom Next Arrow
+const CustomNextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      className="absolute top-[80%] right-5 transform -translate-y-1/2 z-10 cursor-pointer  rounded-full p-2 shadow"
+      onClick={onClick}
+    >
+      <FaChevronRight className="text-[#031c3f] text-xl" />
+    </div>
+  );
+};
+
+// Custom Prev Arrow
+const CustomPrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      className="absolute top-[80%] left-5 transform -translate-y-1/2 z-10 cursor-pointer  rounded-full p-2 shadow"
+      onClick={onClick}
+    >
+      <FaChevronLeft className="text-[#031c3f] text-xl" />
+    </div>
   );
 };
 
