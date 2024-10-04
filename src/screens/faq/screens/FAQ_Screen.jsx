9ab2efb5accc97b8611dcb406a33eba36095eb55
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-10-04 10:12:44 +0530

    add navbar and top header to faq screen

--- src/screens/faq/screens/FAQ_Screen.jsx
+++ src/screens/faq/screens/FAQ_Screen.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import FAQ_Content from "../component/Faq_Content";
 import Footer from "../component/Footer";
+import TopHeader from "../../Homepage/component/TopHeader";
+import Navbar from "../../Homepage/component/Navbar";
 
 function FAQ_Screen() {
   return (
     <div>
+      <TopHeader />
+      <Navbar />
       <FAQ_Content />
       <Footer />
     </div>
