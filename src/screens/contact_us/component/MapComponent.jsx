325ffed83a43d2e2da3a0928726861c6c6ed3f98
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-10-05 00:01:00 +0530

    conclude google map component

--- src/screens/contact_us/component/MapComponent.jsx
+++ src/screens/contact_us/component/MapComponent.jsx
@@ -2,17 +2,23 @@ import React from "react";
 
 const MapComponent = () => {
   return (
-    <div style={{ width: "100%", height: "600px" }}>
-      <iframe
-        width="100%"
-        height="600"
-        frameBorder="0"
-        scrolling="no"
-        marginHeight="0"
-        marginWidth="0"
-        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=New%20York+(New%20York)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-        title="Google Map"
-      />
+    <div>
+      {/* Gray-colored div using Tailwind CSS */}
+      <div className="w-full h-12 bg-gray-200"></div>
+      {/* Google Map iframe */}
+      <div style={{ width: "100%", height: "400px" }}>
+        <iframe
+          width="100%"
+          height="400"
+          frameBorder="0"
+          scrolling="no"
+          marginHeight="0"
+          marginWidth="0"
+          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=New%20York+(New%20York)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          title="Google Map"
+        />
+      </div>
+      <div className="w-full h-6 bg-gray-200"></div>
     </div>
   );
 };
--- src/screens/contact_us/screens/ContactUsScreen.jsx
+++ src/screens/contact_us/screens/ContactUsScreen.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import MapComponent from "../component/MapComponent";
+import TopHeader from "../../Homepage/component/TopHeader";
+import Navbar from "../../Homepage/component/Navbar";
 
 function ContactUsScreen() {
   return (
-    <div>
+    <div className="relative">
+       <TopHeader/>
+       <Navbar/>
       <MapComponent />
     </div>
   );
