d012693e94dc7860eaccf9c0be50b44019a1cfef
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-10-05 11:49:24 +0530

    update social connect component

--- src/screens/contact_us/screens/ContactUsScreen.jsx
+++ src/screens/contact_us/screens/ContactUsScreen.jsx
@@ -4,8 +4,9 @@ import TopHeader from "../../Homepage/component/TopHeader";
 import Navbar from "../../Homepage/component/Navbar";
 import ContactUsForm from "../component/ContactUsForm";
 import ServicesSection from "../component/ServiceSection";
-import LastContent from "../component/LastContent";
+
 import Footer from "../../faq/component/Footer";
+import SocialConnectComponent from "../component/SocialConnectComponent";
 
 function ContactUsScreen() {
   return (
@@ -15,7 +16,7 @@ function ContactUsScreen() {
       <MapComponent />
       <ContactUsForm />
       <ServicesSection />
-      <LastContent />
+      <SocialConnectComponent />
       <Footer />
     </div>
   );
