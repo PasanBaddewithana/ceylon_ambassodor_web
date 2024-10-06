f976e768cdf899dfc8d3d3a0d35676718fc436d7
Min3th <minethweerasinghe@gmail.com>
2024-10-07 02:30:03 +0530

    added second part of Brandpulse

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import ContactUsScreen from "./screens/contact_us/screens/ContactUsScreen";
 import FAQ_Screen from "./screens/faq/screens/FAQ_Screen";
+import BrandPulse from "./screens/Homepage/screen/BrandPulse";
 import HomePage from "./screens/Homepage/screen/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <ContactUsScreen />
+      {/* <ContactUsScreen /> */}
+      {/* <HomePage/> */}
+      <BrandPulse/>
     </div>
   );
 }
---
+++ src/screens/Homepage/component/BrandPulseNavigationSection.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const BrandNavSection = () => {
+  return (
+    <section className="w-[85%] mx-auto mt-6 grid grid-cols-4 text-center  py-4 bg-white font-bold font-kollektif">
+      <div className="relative group cursor-pointer border-b-2 border-[#031c3f] pb-[10px]">
+        <span className="text-[#031c3f] font-Kollektif font-bold group-hover:text-yellow-500">
+          BRAND STRATEGY
+        </span>
+        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-[-2px] w-0 h-[3px] bg-[#031c3f] group-hover:w-[80%] group-hover:bg-[#c3a05f] transition-all duration-300"></div>
+      </div>
+      <div className="relative group cursor-pointer border-b-2 border-[#031c3f] pb-[10px]">
+        <span className="text-[#031c3f] font-bold group-hover:text-yellow-500">
+          DIGITAL MARKETING
+        </span>
+        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-[-2px] w-0 h-[3px] bg-[#031c3f] group-hover:w-[80%] group-hover:bg-[#c3a05f] transition-all duration-300"></div>
+      </div>
+      <div className="relative group cursor-pointer border-b-2 border-[#031c3f] pb-[10px]">
+        <span className="text-[#031c3f] font-bold group-hover:text-yellow-500">
+          CREATIVE DESIGN
+        </span>
+        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-[-2px] w-0 h-[3px] bg-[#031c3f] group-hover:w-[80%] group-hover:bg-[#c3a05f] transition-all duration-300"></div>
+      </div>
+      <div className="relative group cursor-pointer border-b-2 border-[#031c3f] pb-[10px]">
+        <span className="text-[#031c3f] font-bold group-hover:text-yellow-500">
+          CONTENT MARKETING
+        </span>
+        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-[-2px] w-0 h-[3px] bg-[#031c3f] group-hover:w-[80%] group-hover:bg-[#c3a05f] transition-all duration-300"></div>
+      </div>
+    </section>
+  );
+};
+
+export default BrandNavSection;
