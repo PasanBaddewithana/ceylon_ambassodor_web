8fd938ff0b012be9d057efab6bc007d173be2128
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-10-16 00:52:22 +0530

    update fonts in stats

--- src/screens/Homepage/component/StateSection.jsx
+++ src/screens/Homepage/component/StateSection.jsx
@@ -9,7 +9,7 @@ const StatsSection = () => {
   ];
 
   return (
-    <div className="bg-customBlue py-12">
+    <div className="bg-customBlue py-12 font-MontserratMedium ">
       <div className="max-w-7xl mx-auto flex justify-around items-center">
         {stats.map((stat, index) => (
           <div key={index} className="flex items-center space-x-4">
@@ -18,7 +18,7 @@ const StatsSection = () => {
             {/* Stat Block */}
             <div className="text-center text-white">
               <p className="text-sm">{stat.label}</p>
-              <p className="text-4xl font-bold">{stat.value}</p>
+              <p className="text-5xl font-bold">{stat.value}</p>
             </div>
           </div>
         ))}
