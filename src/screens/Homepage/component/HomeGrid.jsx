0d570f21fbf2bc6c23e10d30aafcb6522d843b18
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-10-15 23:51:41 +0530

    update home grid component

--- src/screens/Homepage/component/HomeGrid.jsx
+++ src/screens/Homepage/component/HomeGrid.jsx
@@ -2,19 +2,19 @@ import React from "react";
 
 const HomeGridComponent = () => {
   return (
-    <div className="w-full h-[400px] border border-gray-400">
-      <div className="grid grid-cols-4 divide-x divide-gray-700 h-full">
+    <div className="w-full h-[200px] border border-gray-400">
+      <div className="grid grid-cols-4 divide-x-[2px] divide-customBlue h-full border-t-2 border-customBlue">
         {/* Column 1 */}
-        <div className="bg-gray-300"></div>
+        <div className="bg-customGray"></div>
 
         {/* Column 2 */}
-        <div className="bg-gray-300"></div>
+        <div className="bg-customGray"></div>
 
         {/* Column 3 */}
-        <div className="bg-gray-300"></div>
+        <div className="bg-customGray"></div>
 
         {/* Column 4 */}
-        <div className="bg-gray-300"></div>
+        <div className="bg-customGray"></div>
       </div>
     </div>
   );
