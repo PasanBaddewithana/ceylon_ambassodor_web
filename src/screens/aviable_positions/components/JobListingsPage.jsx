d2c834117cbca67a9a856a4075f2887a505d384c
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-10-16 22:57:09 +0530

    update styles in jobListingspage

--- src/screens/aviable_positions/components/JobListingsPage.jsx
+++ src/screens/aviable_positions/components/JobListingsPage.jsx
@@ -5,31 +5,38 @@ const JobListingsPage = () => {
   return (
     <div className="min-h-screen bg-white">
       {/* Top Section with Image and Text */}
-      <div className="container mx-auto flex flex-col lg:flex-row lg:items-center lg:space-x-8 py-16 px-4 lg:px-8">
+      <div className="container max-w-screen-lg mx-auto flex flex-col lg:flex-row items-center justify-between py-16 px-4 lg:px-8">
         {/* Image Section */}
-        <div className="lg:w-1/2 mb-8 lg:mb-0">
+        <div className="lg:w-1/2 flex justify-center">
           <img
             src={working_man}
             alt="Shipping services"
-            className="w-full h-auto rounded-lg shadow-lg object-cover"
+            className="w-3/4 h-auto  "
           />
         </div>
 
         {/* Text Section */}
-        <div className="lg:w-1/2 flex flex-col justify-center">
-          <h2 className="text-4xl font-bold text-gray-800 mb-4">
-            Fast and professional shipping services
-          </h2>
-          <p className="text-gray-600 mb-6 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            elementum, lacus a cursus hendrerit, lectus risus rhoncus nunc, sit
-            amet accumsan.
-          </p>
-
-          <button className="flex items-center text-white bg-blue-600 px-6 py-3 rounded-lg hover:bg-blue-700 transition-colors">
-            <span className="text-2xl font-bold mr-2">+</span>
-            View more about our services
-          </button>
+        <div className="lg:w-1/2 flex flex-col justify-center border-l-2  mt-8 pb-4 lg:mt-0">
+          <div className="border-b-2 border-bggray pl-8">
+            <h2 className="text-2xl font-bold text-gray-800 mb-4">
+              Fast and professional shipping services
+            </h2>
+            <p className="text-gray-600 mb-6 leading-relaxed">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              elementum, lacus a cursus hendrerit, lectus risus rhoncus nunc,
+              sit amet accumsan.
+            </p>
+          </div>
+          <div className="mt-4 ml-8 flex items-center">
+            <button className="flex items-center">
+              <div className="bg-blue-800 rounded-full w-8 h-8 flex items-center justify-center ">
+                <span className="text-2xl font-bold text-white">+</span>
+              </div>
+            </button>
+            <div className="ml-4">
+              <p>View more about our services</p>
+            </div>
+          </div>
         </div>
       </div>
 
