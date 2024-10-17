23f4658db87621cedc461f06cff11ee6c2fc66ce
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-10-17 09:07:30 +0530

    update  qquaote banner styles

--- src/blog/components/QuaoteBanner.jsx
+++ src/blog/components/QuaoteBanner.jsx
@@ -5,18 +5,19 @@ function QuoteBanner() {
   return (
     <div className="bg-customBlue max-w-5xl mx-auto text-white py-12 px-6 relative">
       {/* Left Quote Icon (Top Left Corner) */}
-      <FaQuoteLeft className="text-5xl text-white absolute top-4 left-4" />
+      <FaQuoteLeft className="text-3xl text-white absolute top-4 left-24" />
 
       {/* Content Container */}
       <div className="max-w-4xl mx-auto text-center flex flex-col items-center gap-4">
-        <p className="text-xl italic font-semibold leading-relaxed">
-          Combining Advanced Technology and Global Networks to Offer Freight
-          Solutions That Keep Your Business Competitive and Growing.
+        <p className="text-xl italic font-bold leading-relaxed">
+          Combining Advanced Technology and Global Networks to Offer
+          <br />
+          Freight Solutions That Keep Your Business Competitive and Growing.
         </p>
       </div>
 
       {/* Right Quote Icon (Bottom Right Corner) */}
-      <FaQuoteRight className="text-5xl text-white absolute bottom-4 right-4" />
+      <FaQuoteRight className="text-3xl text-white absolute bottom-4 right-24" />
     </div>
   );
 }
