ba5e37a048f37c4dcd477c495d4b2b1a477025d3
Baddewithana P <99246737+IT21247804@users.noreply.github.com>
2024-12-21 19:01:23 +0530

    fix(mobile-optimization): founded issues

--- src/screens/faq/screens/FAQ_Screen.jsx
+++ src/screens/faq/screens/FAQ_Screen.jsx
@@ -25,18 +25,26 @@ function FAQ_Screen() {
         
       >
         <Navbar />
-        <div className="mt-10 ml-24" >
-          <h1 className="text-5xl font-Montserrat font-bold text-black" data-aos="fade-up" data-aos-duration="1000">
-            Frequently Asked Questions
-          </h1>
-          <p className="mt-4 text-gray-700" data-aos="fade-up" data-aos-duration="1000">
-            At Ceylon Ambassador, we understand that you may have questions
-            about our services. Below, we’ve compiled some of the most common
-            inquiries to provide you with quick and helpful answers. If you
-            don’t find what you’re looking for, feel free to leave a reply or
-            contact us directly!
-          </p>
-        </div>
+        <div className="mt-10 px-4 md:px-10 lg:ml-24">
+  <h1 
+    className="text-3xl md:text-4xl lg:text-5xl font-Montserrat font-bold text-black" 
+    data-aos="fade-up" 
+    data-aos-duration="1000"
+  >
+    Frequently Asked Questions
+  </h1>
+  <p 
+    className="mt-4 text-gray-700 text-sm md:text-base lg:text-lg" 
+    data-aos="fade-up" 
+    data-aos-duration="1000"
+  >
+    At Ceylon Ambassador, we understand that you may have questions about our 
+    services. Below, we’ve compiled some of the most common inquiries to 
+    provide you with quick and helpful answers. If you don’t find what you’re 
+    looking for, feel free to leave a reply or contact us directly!
+  </p>
+</div>
+
       </div>
 
       {/* Make sure FAQ_Content is placed below Navbar */}
