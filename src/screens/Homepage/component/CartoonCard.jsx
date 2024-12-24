b866afce221cc807495d951d32e4a65c872df2c2
Baddewithana P <99246737+IT21247804@users.noreply.github.com>
2024-12-24 11:10:00 +0530

    fix(cartoon-card): enhance layout and styling for improved visual presentation

--- src/screens/Homepage/component/CartoonCard.jsx
+++ src/screens/Homepage/component/CartoonCard.jsx
@@ -11,23 +11,27 @@ const CartoonCard = ({
   const textColorClass = `text-${textcolor}`;
 
   return (
-    <div className="flex flex-col items-center justify-between w-[200px] h-[300px] gap-2">
-      <img
-        src={image}
-        alt={alt}
-        className="object-contain w-full"
-        style={{ maxHeight: h }}
-      />
-      <p
-        className={`mt-3 font-bold font-MontserratSemiBold text-center text-[16px] ${textColorClass} min-h-[40px]`}
-      >
-        {topic}
-      </p>
-      <p
-        className={`text-center font-Montserrat text-xs ${textColorClass} min-h-[60px] line-clamp-3`}
-      >
-        {content}
-      </p>
+    <div className="flex flex-col items-center justify-between w-[300px] h-[350px] gap-2 p-4 bg-white shadow-md rounded-lg">
+      <div className="flex-shrink-0">
+        <img
+          src={image}
+          alt={alt}
+          className="object-contain w-200 h-150"
+          style={{ maxHeight: h }}
+        />
+      </div>
+      <div className="flex flex-col items-center justify-center flex-grow">
+        <p
+          className={`mt-3 font-bold font-MontserratSemiBold text-center text-[16px] ${textColorClass} min-h-[40px]`}
+        >
+          {topic}
+        </p>
+        <p
+          className={`text-center font-Montserrat text-xs ${textColorClass} min-h-[60px]`} //line-clamp-3
+        >
+          {content}
+        </p>
+      </div>
     </div>
   );
 };
